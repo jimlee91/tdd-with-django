a6adaf675e6e02003a6f484c2b84c911aa635260
skyrocket <skyrocket.kr@gmail.com>
2020-03-16 21:56:38 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리팩터링

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import reverse, resolve
 
@@ -16,7 +17,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        print(response)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('lists/home.html')
+        self.assertEqual(response.content.decode(), expected_html)
---
+++ lists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'lists'
+urlpatterns = [
+    path('', views.home_page, name='home_page'),
+]
