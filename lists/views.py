87a8755a19addac668ffc8e3ac2364c03f531f77
skyrocket <skyrocket.kr@gmail.com>
2020-03-16 21:18:06 +0900

    최소 html 을 반환하는 기본 뷰

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page(request):
-    pass
+    return HttpResponse("<html><title>To-Do lists</title></html>")
