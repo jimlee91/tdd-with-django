530cf01a9e1379f943befda70d92df16079c9c0d
skyrocket <skyrocket.kr@gmail.com>
2020-03-16 21:44:33 +0900

    기능테스트가 작업 아이템을 추가할수 있는지 확인 코드 추가

--- functional_text.py
+++ functional_text.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,28 +18,44 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        # 브라우저의 타이틀이 'To-Do'인지 확인
+        # 브라우저의 타이틀과 헤더가 'To-Do'인지 확인
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), '작업 아이템 입력'
+        )
+
+        # '공작깃털 사기'라고 텍스트 상자에 입력
+        inputbox.send_keys('공작깃털 사기')
+
+        # 엔터키를 치면 페이지가 갱신되고
+        inputbox.send_keys(keys.ENTER)
+
+        # 작업목록에 "1: 공작깃털 사기" 추가
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+
         self.fail('Finish the Test!!!')
 
+    # 추가 아이템을 입력할수 있는 텍스트 상자 존재
 
-# '공작깃털 사기'라고 텍스트 상자에 입력
+    # '공작깃털을 이용해서 그물 만들기'라고 텍스트 상자에 입력
 
-# 엔터키를 치면 페이지가 갱신되고 작업목록에 "1: 공작깃털 사기" 추가
+    # 페이지 갱신, 두개의 목록
 
-# 추가 아이템을 입력할수 있는 텍스트 상자 존재
+    # 입력한 목록이 저장 됐는지 확인
 
-# '공작깃털을 이용해서 그물 만들기'라고 텍스트 상자에 입력
+    # 특정 URL 생성
 
-# 페이지 갱신, 두개의 목록
+    # URL에 대한 설명도 추가
 
-# 입력한 목록이 저장 됐는지 확인
-
-# 특정 URL 생성
-
-# URL에 대한 설명도 추가
-
-# 작업목록이 그대로 있는지 확인
+    # 작업목록이 그대로 있는지 확인
 
 
 if __name__ == '__main__':
