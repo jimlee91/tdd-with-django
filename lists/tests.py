c647f60a763b29a28780c3a37f90e08fd7e1772c
skyrocket <skyrocket.kr@gmail.com>
2020-03-16 21:03:52 +0900

    첫 단위테스트와 url 맵핑

--- lists/tests.py
+++ lists/tests.py
@@ -11,3 +11,8 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_match_resolve_path_with_reverse_url_name(self):
+        resolve_path = resolve('/')
+        reverse_url_name = reverse('lists:home_page')
+        self.assertEqual(resolve_path, reverse_url_name)
