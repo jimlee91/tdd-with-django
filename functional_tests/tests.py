ee0710c6a47cb0c360afb2f6206c349c0b0c6637
skyrocket <skyrocket.kr@gmail.com>
2020-03-17 00:06:16 +0900

    tests 앱 생성 및 liveservertestcase 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         # 웹 브라우저 접속
@@ -21,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 브라우저의 타이틀과 헤더가 'To-Do'인지 확인
         self.assertIn('To-Do', self.browser.title)
@@ -66,7 +67,3 @@ class NewVisitorTest(unittest.TestCase):
         # 작업목록이 그대로 있는지 확인
 
         self.fail('Finish the Test!!!')
-
-
-if __name__ == '__main__':
-    unittest.main()
